63cc6359f4f95ff648ee4091b066308bd9d7ab3b
QuackCoding <sakuuddinlola4142@gmail.com>
2021-06-25 10:53:40 +0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import CryptoTokenAuth
+from setuptools import setup, find_packages
+
+
+version = CryptoTokenAuth.__version__
+
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+
+setup(
+    name="CryptoTokenAuth",
+    version=version,
+    author="Aiden (Dev)",
+    author_email="",
+    long_description_content_type="text/markdown",
+    long_description=read_file('README.md'),
+    description='A token authentication method.',
+    packages=find_packages(),
+    url='https://github.com/QuackCoding/CryptoTokenAuth',
+    package_data={'CryptoTokenAuth': [version]},
+    install_requires=['cryptography'],
+    keywords=['TokenAuthentication', 'CryptoTokenAuth', 'Authentication', 'Token'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
